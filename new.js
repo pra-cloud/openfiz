8cb339caf4630a120865acde7dd239ddf958e6b9
pure5 <tenserprateek@gmail.com>
2020-05-18 22:29:49 +0530

    hello node js

---
+++ new.js
@@ -0,0 +1,15 @@
+var express = require('express');
+app = express();
+
+app.get('/',function(req,res) {
+res.send('hello');[D[D\n[C[D[D[D[D[[C[C[C');
+});
+
+app.get('/',function(req,res) {
+res.send('hello \n');
+});
+[D[D[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C user\n');[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+
+app.listen(8080,function () {
+console.log("hiiii 8080");
+});
